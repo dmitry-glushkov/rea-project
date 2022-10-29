ae219e0e60884be1d1f41c4594821fa5396037ea
Glushkov Dmitriy Evgenyevich <dmglushkov@ozon.ru>
2022-10-29 14:12:18 +0300

    project mock + init project page

--- internal/handlers/get_project.go
+++ internal/handlers/get_project.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"net/http"
-	"ucheba/back/internal/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,17 +23,38 @@ func (impl *Implementation) GetProject() gin.HandlerFunc {
 			return
 		}
 
-		var project models.Project
-		project, err = models.GetProject(c.Request.Context(), impl.DB, r.PID)
-		if err != nil {
-			c.String(http.StatusInternalServerError, err.Error())
-			return
-		}
+		// var project models.Project
+		// project, err = models.GetProject(c.Request.Context(), impl.DB, r.PID)
+		// if err != nil {
+		// 	c.String(http.StatusInternalServerError, err.Error())
+		// 	return
+		// }
+
+		project := createMockProject(r.PID)
 
 		c.JSON(http.StatusOK, GetProjectResponse{
-			Project: ProjectInfo{
-				PID: project.ID,
-			},
+			Project: project,
 		})
+	}
+}
+
+func createMockProject(pid int) ProjectInfo {
+	return ProjectInfo{
+		PID:   pid,
+		Owner: "создатель проекта",
+		Desc: `
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		большое описание проекта большое описание проекта большое описание проекта большое описание проекта большое описание проекта
+		`,
+		ProjectName: "название проекта",
+		Sum:         100000,
+		Target:      400000,
 	}
 }
