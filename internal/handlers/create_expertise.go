a6ad54dc3a75bcebc4085ae11eeebb60ad450550
Glushkov Dmitriy Evgenyevich <dmglushkov@ozon.ru>
2022-11-09 22:19:14 +0300

    y

---
+++ internal/handlers/create_expertise.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"net/http"
+	"ucheba/back/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CreateExperiseRequest struct {
+	Pid     int    `json:"pid"`
+	Content string `json:"content"`
+}
+
+type CreateExpertiseResponse struct{}
+
+func (impl *Implementation) CreateExpertise() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		r := &CreateExperiseRequest{}
+		err := c.Bind(r)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		exp := models.Expertise{
+			Pid:     r.Pid,
+			Content: r.Content,
+		}
+		// err = exp.Save(c.Request.Context(), impl.DB)
+		err = exp.SaveMock(c.Request.Context(), impl.DB) // todo mock
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		c.JSON(http.StatusOK, CreateExpertiseResponse{})
+	}
+}
---
+++ internal/models/expertise.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v4"
+)
+
+type Expertise struct {
+	ID      int    `json:"id"`
+	Pid     int    `json:"pid"`
+	Content string `json:"content"`
+}
+
+func (exp Expertise) Save(ctx context.Context, db *pgx.Conn) error {
+	return nil
+}
+
+func (exp Expertise) SaveMock(ctx context.Context, db *pgx.Conn) error {
+	return nil
+}
+
+func GetExpertises(ctx context.Context, db *pgx.Conn, pid int) ([]Expertise, error) {
+	return nil, nil
+}
+
+func GetExpertisesMock(ctx context.Context, db *pgx.Conn, pid int) ([]Expertise, error) {
+	return []Expertise{
+		{
+			ID:      0,
+			Pid:     pid,
+			Content: "эксперт экспертиза эксперт экспертиза эксперт экспертиза эксперт экспертиза эксперт экспертиза эксперт экспертиза эксперт экспертиза эксперт экспертиза эксперт экспертиза эксперт экспертиза ",
+		},
+	}, nil
+}
